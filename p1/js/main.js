047e6e0b3199b008f2883b61bb386a12b1d35b82
alannarisse <alannarisse@users.noreply.github.com>
2026-05-06 17:39:00 -0700

    added jquery and a mess

---
+++ p1/js/main.js
@@ -0,0 +1,10 @@
+// gsap.utils.toArray('section').forEach((panel,i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: 'top top',
+//     pin: true,
+//     pinSpacing: false // this makes sure that if the panels are different heights, there's no gap
+//   });
+// });
+ 
+
